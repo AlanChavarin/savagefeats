9f0cf76a6d4bc885d8b48177848ccdb7042f1e87
AlanChavarin <alanchavarin4@hotmail.com>
2024-01-19 05:40:39 -0800

    more responsive design to home page hero

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,10 +4,10 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/HomepageComponents/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -23,6 +23,9 @@ const config: Config = {
       fontFamily: {
         custom: ['foulfiend', 'sans-serif'], // 'CustomFont' is the font name
       },
+      screens: {
+        'ssm': "533px"
+      }
     },
   },
   plugins: [],
